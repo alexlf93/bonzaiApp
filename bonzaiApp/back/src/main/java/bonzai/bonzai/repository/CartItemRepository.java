94bc459a7846de585890d2610f2c93d548a9dbb4
alexscript <alejandro.93lf@gmail.com>
2023-09-17 16:52:14 +0200

    backend finished

--- bonzaiApp/back/src/main/java/bonzai/bonzai/repository/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package bonzai.bonzai.repository;
-import bonzai.bonzai.model.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
